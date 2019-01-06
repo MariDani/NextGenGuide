3899e691b74c56127eb3273ac6561fd6fecbfb1d
Zuzana Arnoldova <zuzana.arnoldova@merck.com>
2019-01-06 16:47:16 +0100

    Remove unused code in db

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,5 @@
 const pg = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432';
-
 const client = new pg.Client({
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
