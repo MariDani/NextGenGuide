fcbe7c411d91c74fc3460a24a69496a26795a600
Mariana Danielová <danielova.mariana@gmail.com>
2019-01-12 14:15:39 +0100

    Server fix for CORS (not working)

--- server/index.js
+++ server/index.js
@@ -4,6 +4,13 @@ var express = require("express");
 var app = express();
 const db = require('./db')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:2015");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+  next();
+});
+
 app.get('/absolvents', (req, res, next) => {
   db.query('SELECT * FROM absolvents', (err, dbRes) => {
     if (err) {
