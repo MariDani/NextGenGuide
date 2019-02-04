c228f038a3a4bad2b570b6a75785aaa395e159d8
Mariana Danielová <danielova.mariana@gmail.com>
2019-02-04 18:06:25 +0100

    Add connection string

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,11 @@
 const pg = require('pg');
 
 const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,    
-  database: 'next-gen-guide-development',
+  database: process.env.DB_NAME,
   port: 5432,
 })
 
