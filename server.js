d1cbb595859116ff930ce8fda147a1fd6d4a7c5f
Mariana Danielová <danielova.mariana@gmail.com>
2019-03-05 19:29:06 +0100

    Server.js updated

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:2015");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  });
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
