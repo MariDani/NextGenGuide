491271c5d8bcecb44b368e6b065ad132b01cbf20
Mariana Danielová <danielova.mariana@gmail.com>
2019-03-05 19:39:25 +0100

    CORS updated

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://next-gen-web.herokuapp.com/");
+    res.header("Access-Control-Allow-Origin", "*");
     // res.header("Access-Control-Allow-Origin", "http://localhost:2015");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Credentials", false);
     next();
   });
 app.use(serveStatic(path.join(__dirname, 'dist')));
