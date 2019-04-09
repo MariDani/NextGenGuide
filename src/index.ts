a4111f34566c6b34d01dcdb0c3e80d48a3635e8a
Mariana Danielová <danielova.mariana@gmail.com>
2019-04-09 19:55:12 +0200

    Added health check server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import MentorCards from "./mentorCards";
 import MentorDetail from "./mentorDetail";
+import axios from "axios";
 
 const dbUrl = "https://next-gen.herokuapp.com"//"http://localhost:3000/mentors";
+
+axios({ method: "get", url: "https://next-gen.herokuapp.com/health", withCredentials: true });
 
 if (document.getElementById("mentordetails")) {
     const regex = /[?](\w+)/g;
